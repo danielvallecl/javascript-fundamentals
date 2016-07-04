9982845b43e9c22a638b889812326d9701162564
Daniel Valle <daniel.vallecl@gmail.com>
2016-07-03 21:19:50 -0400

    Finished Objects

--- objects.js
+++ objects.js
@@ -32,7 +32,17 @@ console.log(recipe.ingredients);
 // Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
 // Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
 
+var books = [
+  {title: "The Divine Comedy", author: "Dante Alighieri", alreadyRead: true},
+  {title: "War & Peace", author: "Leon Tolstoy", alreadyRead: false}
+]
 
+for (i = 0; i < books.length; i++) {
+  if (books[i].alreadyRead === true) {
+    console.log("You have read " + books[i].title + " by " + books[i].author)
+}  else
+    console.log("You have not read " + books[i].title + " by " + books[i].author);
+};
 
 
 // EXERCISE: The Movie Database
@@ -41,3 +51,11 @@ console.log(recipe.ingredients);
 
 // Create an object to store the following information about your favorite movie: title (a string), duration (a number), and stars (an array of strings).
 // Create a function to print out the movie information like so: "Puff the Magic Dragon lasts for 30 minutes. Stars: Puff, Jackie, Living Sneezes."
+
+favourite_movie = {title: "Mouchette", director: "Robert Bresson", duration: 90, cast: ["Nadine Nortier", " Jean-Claude Guilbert"]}
+
+function print_movie(movie_title) {
+  console.log(favourite_movie.title + " lasts for " + favourite_movie.duration + "minutes." + " Stars: " + favourite_movie.cast);
+};
+
+print_movie(favourite_movie)
